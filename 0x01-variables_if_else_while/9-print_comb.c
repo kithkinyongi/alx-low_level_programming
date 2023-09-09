4a3016ffb06d4a88b927a7c3312f2a1e2894e801
keith kiptanui <kithkinyongi@gmail.com>
2023-09-09 15:51:39 -0700

    single

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: 0 success
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48 ; i <= 57 ; i++)
+	{
+		putchar(i);
+		if (i == 57)
+			putchar('$');
+		else
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	return (0);
+}
