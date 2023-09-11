4bd261cc105f95f260192420d194d8d5d8f8c73a
keith kiptanui <kithkinyongi@gmail.com>
2023-09-11 16:31:38 -0700

    possible 3 digit number

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int i = 48;
+	int k;
+
+	while (i <= 57)
+	{
+		int j = i + 1;
+
+		while (j <= 57)
+		{
+			for (k = j + 1; k <= 57 ; k++)
+			{
+				if (i != j && j != k && i != k)
+				{
+					putchar(i);
+					putchar(j);
+					putchar(k);
+					if (i != 54 || j != 56 || k != 57)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+					else
+						putchar('\n');
+				}
+			}
+			j++;
+		}
+		i++;
+	}
+	return (0);
+}
