b625a14825cfc93ddc4b906c092d19463fbdb2ae
keith kiptanui <kithkinyongi@gmail.com>
2023-09-21 17:11:40 -0700

    string compare :)

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,14 +10,16 @@
 int _strcmp(char *s1, char *s2)
 {
 	int i;
+	int j = 0;
 
 	for (i = 0 ; s1[i] != '\0' || s2[i] != '\0' ; i++)
 	{
 		if (s1[i] > s2[i])
-			return (15);
+			return (j = 15);
 		else if (s2[i] > s1[i])
-			return (-15);
+			return (j = -15);
 		else
-			return (0);
+			return (j = 0);
 	}
+	return (j);
 }
