8f2608771734f385b5fc39645a4506c5ff4411fd
keith kiptanui <kithkinyongi@gmail.com>
2023-09-28 14:23:47 -0700

    string len :)

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strlen_recursion - outputs the size of a string
+ * @s: string to be evaluated
+ *
+ * Return: size of the string
+ */
+int _strlen_recursion(char *s)
+{
+	static int j = 1;
+
+	if (s[0] != '\0')
+	{
+		j++;
+		_strlen_recursion(s + 1);
+	}
+	return (j - 1);
+}
